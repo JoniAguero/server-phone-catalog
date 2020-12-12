76dd0974f0c71e1dd028b37136dae407c8c1bfae
Jonathan Aguero <jonathan@eelptex.com>
2020-12-12 10:44:47 +0100

    circleci configuration added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3100;
+
+app.use('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));

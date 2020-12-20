1d98ced699ebe103a190590fdcd74d85e1e7ab99
Jonathan Aguero <jonathan@eelptex.com>
2020-12-20 20:10:12 +0100

    error 500 enabled

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -34,7 +34,7 @@ const createUser = async (req, res = response) => {
   } catch (error) {
     res.status(500).json({
       ok: false,
-      msg: 'Please talk to the administrator',
+      msg: error,
     })
   }
 }
@@ -74,7 +74,7 @@ const loginUser = async (req, res = response) => {
   } catch (error) {
     res.status(500).json({
       ok: false,
-      msg: 'Please talk to the administrator',
+      msg: error,
     })
   }
 }

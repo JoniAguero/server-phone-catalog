6726ca37df8b3f49ece7a2957bbb0c9f5ec3ded9
Joni <jonathan@eelptex.com>
2020-12-21 19:54:24 +0100

    validatefields middleware added

--- routes/phones.route.js
+++ routes/phones.route.js
@@ -1,16 +1,60 @@
 "use strict"
 
-const { Router } = require('express');
-const { validarToken } = require('../middlewares/validate-token')
+const { Router } = require("express")
+const { validarToken } = require("../middlewares/validate-token")
+const { check } = require("express-validator")
+const { validateFields } = require("../middlewares/validate-fields")
 
-const PhonesController = require('../controllers/phones.controller');
-const phoneRoute = Router();
+const PhonesController = require("../controllers/phones.controller")
+const phoneRoute = Router()
 
-phoneRoute.post('/phones', validarToken, PhonesController.CreatePhone);
-phoneRoute.post('/phones/upload-image/:id', validarToken, PhonesController.UploadImagePhone);
-phoneRoute.put('/phones/:id', validarToken, PhonesController.UpdatePhone);
-phoneRoute.delete('/phones/:id', validarToken, PhonesController.DeletePhone);
-phoneRoute.get('/phones', PhonesController.GetPhones);
-phoneRoute.get('/phones/:id', PhonesController.GetPhoneById);
+phoneRoute.post(
+  "/phones",
+  [
+    check("name", "The name is required").not().isEmpty(),
+    check("manufacturer", "The manufacturer is required").not().isEmpty(),
+    check("description", "The description is required").not().isEmpty(),
+    check("color", "The color is required").not().isEmpty(),
+    check("price", "The price is required").not().isEmpty(),
+    check("screen", "The screen is required").not().isEmpty(),
+    check("cameraFront", "The Camera Front is required").not().isEmpty(),
+    check("cameraBack", "The Camera Back is required").not().isEmpty(),
+    check("batery", "The batery is required").not().isEmpty(),
+    check("storage", "The storage is required").not().isEmpty(),
+    check("processor", "The processor is required").not().isEmpty(),
+    check("ram", "The ram is required").not().isEmpty(),
+    validateFields,
+  ],
+  validarToken,
+  PhonesController.CreatePhone
+)
+phoneRoute.post(
+  "/phones/upload-image/:id",
+  validarToken,
+  PhonesController.UploadImagePhone
+)
+phoneRoute.put(
+  "/phones/:id",
+  [
+    check("name", "The name is required").not().isEmpty(),
+    check("manufacturer", "The manufacturer is required").not().isEmpty(),
+    check("description", "The description is required").not().isEmpty(),
+    check("color", "The color is required").not().isEmpty(),
+    check("price", "The price is required").not().isEmpty(),
+    check("screen", "The screen is required").not().isEmpty(),
+    check("cameraFront", "The Camera Front is required").not().isEmpty(),
+    check("cameraBack", "The Camera Back is required").not().isEmpty(),
+    check("batery", "The batery is required").not().isEmpty(),
+    check("storage", "The storage is required").not().isEmpty(),
+    check("processor", "The processor is required").not().isEmpty(),
+    check("ram", "The ram is required").not().isEmpty(),
+    validateFields,
+  ],
+  validarToken,
+  PhonesController.UpdatePhone
+)
+phoneRoute.delete("/phones/:id", validarToken, PhonesController.DeletePhone)
+phoneRoute.get("/phones", PhonesController.GetPhones)
+phoneRoute.get("/phones/:id", PhonesController.GetPhoneById)
 
-module.exports = phoneRoute;
+module.exports = phoneRoute
